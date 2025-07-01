8213f384fe891dd46521db86b41a3eddf9eca036
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-07-02 04:38:31 +0530

    fix: minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,77 +3,120 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="relative w-full min-h-screen px-44 bg-black text-white flex flex-col justify-center items-center overflow-hidden">
-      {/* Background layers */}
-      <div className="absolute inset-0 h-full bg-[url('/grill.png')] bg-cover bg-center opacity-30 pointer-events-none z-0" />
-      <div className="absolute inset-0 bg-[url('/bg-light.png')] bg-cover bg-top-right pointer-events-none z-0" />
+    <main className="relative w-full min-h-screen bg-black text-white overflow-hidden flex flex-col items-center justify-center">
+      <section className="relative w-full min-h-screen px-44 flex flex-col items-center justify-center">
+        {/* Background layers */}
+        <div className="absolute inset-0 z-0 bg-[url('/grill.png')] bg-cover bg-center opacity-30 pointer-events-none" />
+        <div className="absolute inset-0 z-0 bg-[url('/bg-light.png')] bg-cover bg-top-right pointer-events-none" />
 
-      {/* Grid overlays */}
-      <div className="absolute top-24 left-24 w-[0.05rem] h-full bg-neutral-900/50" />
-      {/* <div className="absolute top-24 left-24 w-full h-[32rem] opacity-40 bg-gradient-to-r from-neutral-900 to-transparent border-y border-neutral-700/50" /> */}
-      <div className="absolute top-[37.9rem] left-0 w-full h-[0.05rem] bg-neutral-900/50" />
-      <div className="absolute right-24 h-full w-[0.05rem] bg-neutral-900/50" />
+        {/* Guide lines */}
+        <div className="absolute top-24 left-24 w-[0.05rem] h-full bg-neutral-900" />
+        <div className="absolute bottom-18 left-0 w-full h-[0.05rem] bg-neutral-900" />
+        <div className="absolute right-24 top-0 h-full w-[0.05rem] bg-neutral-900" />
 
-      {/* Top bar */}
-      <div className="absolute top-8 left-0 w-full px-44 flex justify-between items-end z-10">
-        <Link href="/">
-          <span className="text-3xl font-medium font-mono">OpenSea</span>
-        </Link>
-        <Button
-          label="Get started"
-          href="/home"
-          className="z-10 transition duration-300 shadow-lg hover:shadow-xl"
-        />
-      </div>
+        {/* Header row */}
+        <div className="absolute top-8 left-0 w-full px-44 flex justify-between items-end z-10">
+          <Link href="/">
+            <span className="text-3xl font-medium font-mono">OSSean</span>
+          </Link>
+          <div className="flex gap-4 items-center z-10">
+            {/* GitHub Button */}
+            <div className='z-10 flex justify-between items-center gap-4'>
+              <Link
+                href="https://github.com/faizshaikh17/opensea"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group relative z-50 ml-auto flex w-full max-w-[15.3rem] items-center justify-center gap-2 whitespace-pre rounded-none border border-input border-neutral-700 bg-transparent px-4 py-2 text-sm font-medium text-black shadow-sm transition-all duration-300 ease-out hover:bg-accent hover:text-accent-foreground overflow-hidden hover:ring-black dark:text-white md:flex"
+              >
+                <span className="absolute right-0 h-10 w-8 translate-x-12 overflow-hidden rotate-12 bg-yellow-300 opacity-20 transition-all duration-1000 ease-out group-hover:-translate-x-40 dark:bg-white/60" />
 
-      {/* Main content */}
-      <div className="flex w-full justify-between items-center z-10">
-        {/* Left section */}
-        <div className="flex flex-col gap-5 w-full items-start justify-center z-10">
-          <div className="text-left text-3xl sm:text-5xl font-medium leading-[100%] animate-fade-in w-fit z-10">
-            <span className="bg-gradient-to-r space-x-2 text-white/90">
-              <span>One</span>
-              <span>Click</span>
-              <span>To</span>
-              <span>Open</span>
-              <span>Source</span>
-              <br />
-              <span>Built</span>
-              <span>To</span>
-              <span>Save</span>
-              <span>Your</span>
-              <span>Time</span>
-            </span>
+                <div className="flex items-center ml-2">
+                  <svg
+                    className="w-4 h-4 fill-current"
+                    viewBox="0 0 438.549 438.549"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M409.132 114.573c-19.608-33.596-46.205-60.194-79.798-79.8-33.598-19.607-70.277-29.408-110.063-29.408-39.781 0-76.472 9.804-110.063 29.408-33.596 19.605-60.192 46.204-79.8 79.8C9.803 148.168 0 184.854 0 224.63c0 47.78 13.94 90.745 41.827 128.906 27.884 38.164 63.906 64.572 108.063 79.227 5.14.954 8.945.283 11.419-1.996 2.475-2.282 3.711-5.14 3.711-8.562 0-.571-.049-5.708-.144-15.417a2549.81 2549.81 0 01-.144-25.406l-6.567 1.136c-4.187.767-9.469 1.092-15.846 1-6.374-.089-12.991-.757-19.842-1.999-6.854-1.231-13.229-4.086-19.13-8.559-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-.951-2.568-2.098-3.711-3.429-1.142-1.331-1.997-2.663-2.568-3.997-.572-1.335-.098-2.43 1.427-3.289 1.525-.859 4.281-1.276 8.28-1.276l5.708.853c3.807.763 8.516 3.042 14.133 6.851 5.614 3.806 10.229 8.754 13.846 14.842 4.38 7.806 9.657 13.754 15.846 17.847 6.184 4.093 12.419 6.136 18.699 6.136 6.28 0 11.704-.476 16.274-1.423 4.565-.952 8.848-2.383 12.847-4.285 1.713-12.758 6.377-22.559 13.988-29.41-10.848-1.14-20.601-2.857-29.264-5.14-8.658-2.286-17.605-5.996-26.835-11.14-9.235-5.137-16.896-11.516-22.985-19.126-6.09-7.614-11.088-17.61-14.987-29.979-3.901-12.374-5.852-26.648-5.852-42.826 0-23.035 7.52-42.637 22.557-58.817-7.044-17.318-6.379-36.732 1.997-58.24 5.52-1.715 13.706-.428 24.554 3.853 10.85 4.283 18.794 7.952 23.84 10.994 5.046 3.041 9.089 5.618 12.135 7.708 17.705-4.947 35.976-7.421 54.818-7.421s37.117 2.474 54.823 7.421l10.849-6.849c7.419-4.57 16.18-8.758 26.262-12.565 10.088-3.805 17.802-4.853 23.134-3.138 8.562 21.509 9.325 40.922 2.279 58.24 15.036 16.18 22.559 35.787 22.559 58.817 0 16.178-1.958 30.497-5.853 42.966-3.9 12.471-8.941 22.457-15.125 29.979-6.191 7.521-13.901 13.85-23.131 18.986-9.232 5.14-18.182 8.85-26.84 11.136-8.662 2.286-18.415 4.004-29.263 5.146 9.894 8.562 14.842 22.077 14.842 40.539v60.237c0 3.422 1.19 6.279 3.572 8.562 2.379 2.279 6.136 2.95 11.276 1.995 44.163-14.653 80.185-41.062 108.068-79.226 27.88-38.161 41.825-81.126 41.825-128.906-.01-39.771-9.818-76.454-29.414-110.049z" />
+                  </svg>
+                  <span className="ml-2 text-black dark:text-white">Star on GitHub</span>
+                </div>
 
-            <div
-              className="mt-3 px-2 py-1 w-fit text-sm font-medium text-neutral-500 tracking-tight border-[2px] transition duration-300 max-sm:px-3 max-sm:py-1 max-sm:text-sm z-10"
-              style={{
-                borderImage:
-                  'conic-gradient(#d4d4d4 0deg, #171717 90deg, #d4d4d4 180deg, #171717 270deg, #d4d4d4 360deg) 1',
-              }}
-            >
-              Discover OSS projects perfectly tailored to your interests.
+                <div className="ml-2 flex items-center gap-2 text-sm md:flex">
+                  <svg
+                    className="h-4 w-4 text-gray-500 transition-all duration-300 group-hover:text-yellow-300"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                    />
+                  </svg>
+                  <span className="inline-block font-mono pr-2 font-medium tabular-nums tracking-wider text-black dark:text-white">
+                    0.001k+
+                  </span>
+                </div>
+              </Link>
+
+              <Button
+                label="Get started"
+                href="/home"
+                className="z-10 transition duration-300 shadow-lg hover:shadow-xl"
+              />
             </div>
           </div>
-
-          <div className="text-neutral-400 font-medium leading-[100%] transition duration-300 max-sm:px-3 max-sm:py-1 max-sm:text-sm z-10">
-            <p>
-              Revolutionized how people <br />
-              find open source projects, <br />
-              making it more personal and relevant.
-            </p>
-          </div>
-
-          <Button
-            label="Get started"
-            href="/home"
-            className="z-10 transition duration-300 shadow-lg hover:shadow-xl"
-          />
         </div>
 
-        {/* Right section */}
-        <div className="w-full h-[22.5rem] bg-[url('/worldMap.svg')] bg-cover bg-[position:top] opacity-70 pointer-events-none overflow-x-visible z-10" />
-      </div>
+        {/* Hero Content */}
+        <div className="flex w-full justify-between items-center z-10">
+          <div className="flex flex-col gap-5 w-full items-start z-10">
+            <div className="text-left text-3xl sm:text-5xl font-medium leading-[100%] animate-fade-in w-fit">
+              <span className="bg-gradient-to-r space-x-2 text-white/90">
+                <span>Ocean</span>
+                {/* <span>Click</span> */}
+                <span>Of</span>
+                <span>Open</span>
+                <span>Source</span>
+                <br />
+                <span>Built</span>
+                <span>To</span>
+                <span>Save</span>
+                <span>Your</span>
+                <span>Time</span>
+              </span>
+              <div
+                className="mt-3 px-2 py-1 w-fit text-sm font-medium text-neutral-500 tracking-tight border-[2px] transition duration-300"
+                style={{
+                  borderImage:
+                    'conic-gradient(#d4d4d4 0deg, #171717 90deg, #d4d4d4 180deg, #171717 270deg, #d4d4d4 360deg) 1',
+                }}
+              >
+                Discover OSS Projects.
+              </div>
+            </div>
+
+            <div className="text-neutral-400 font-medium leading-5 transition duration-300">
+              <p>
+                Revolutionized how people <br />
+                find open source projects, <br />
+                making it more personal and relevant.
+              </p>
+            </div>
+
+            <Button
+              label="Get started"
+              href="/home"
+              className="z-10 transition duration-300 shadow-lg hover:shadow-xl"
+            />
+          </div>
+
+          {/* Right visual map */}
+          <div className="w-full h-[22.5rem] bg-[url('/worldMap.svg')] bg-cover bg-top opacity-70 pointer-events-none z-10" />
+        </div>
+      </section>
     </main>
   );
 }
