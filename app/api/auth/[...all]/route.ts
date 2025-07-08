0d18cf9333755cc2523cf2e19fabd5585c1463d7
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-07-08 18:18:11 +0530

    fix:auth on prod 2

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,9 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
+if (typeof window === "undefined") {
+  console.log("✅ DATABASE_URL used:", process.env.DATABASE_URL);
+}
+
+
 export const { GET, POST } = toNextJsHandler(auth);
